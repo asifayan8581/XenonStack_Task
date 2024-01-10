dedae0c1c84e7961fe5882af6d5004667f0bfd12
asifayan8581 <84224649+asifayan8581@users.noreply.github.com>
2024-01-10 17:15:45 +0530

    Add files via upload

---
+++ about-us.php
@@ -0,0 +1,36 @@
+<?php
+include ("header.php");
+?>
+
+<div class="breadcrumb-area gray-bg">
+            <div class="container">
+                <div class="breadcrumb-content">
+                    <ul>
+                        <li><a href="<?php echo FRONT_SITE_PATH?>shop">Home</a></li>
+                        <li class="active">About us </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="about-us-area pt-50 pb-100">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-7 d-flex align-items-center">
+                        <div class="overview-content-2">
+                            <h2>Welcome To <span>Dark Town</span> Cafe !</h2>
+                            <p>It’s just a moment in time – just one hand reaching over the counter to present a cup to another outstretched hand.
+                               <br>But it’s a connection....
+                               <br>We make sure everything we do honors that connection – from our commitment to the highest quality coffee in the world, to the way we engage with our customers and communities to do business responsibly.
+                               <br>From our beginnings as a single store over forty years ago, in every place that we’ve been, and every place that we touch, we've tried to make it a little better than we found it.
+</p>
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+
+<?php
+include("footer.php");
+?>
